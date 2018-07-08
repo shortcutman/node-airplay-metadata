0c0dd57d6e08119d6ad9aebb95e014224ec75e70
Daniel Burke <danieljburke@me.com>
2018-07-08 17:13:13 +1000

    first attempt at pipes

--- airplay-metadata.js
+++ airplay-metadata.js
@@ -1,8 +1,21 @@
 var http = require('http');
+var pipedata = "";
+
+const fs = require('fs');
+const path = require('path');
+const fifopath = path.resolve('/tmp/shairport-sync-metadata');
+const fifo = fs.createReadStream(fifopath);
+fifo.on('data', data => {
+		pipedata = data
+});
+
+
 
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.write('Hello World!\n');
-	res.write('Being accessed at: ' + req.url);
+	res.write("Being accessed at: " + req.url + "\n");
+	res.write(pipedata);
 	res.end();
 }).listen(80);
+
